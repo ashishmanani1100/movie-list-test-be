0d5b1737e11c47f5f21bf3b586970857effcca12
Ashish Manani <ashishmanani1100@gmail.com>
2024-08-22 12:37:50 +0530

    Improved API documentation

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { applyMiddleware } = require('graphql-middleware');
 const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
-const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+const { ApolloServerPluginLandingPageProductionDefault } = require('apollo-server-core');
 const formatError = require('./utils/formatError');
 const models = require('./models');
 const shield = require('./shield');
@@ -29,7 +29,10 @@ const server = new ApolloServer({
   schema: applyMiddleware(graphqlSchema, shield), // graphql shcema with shield authentication
   cache: 'bounded', // prevent memory overflow and ensure efficient caching
   formatError, // handle graphql errors
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageGraphQLPlayground()], // drain http server and enable playground
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    ApolloServerPluginLandingPageProductionDefault({ embed: true }),
+  ], // drain http server and enable playground
   context: async ({ req, res }) => {
     return { req, res, models };
   },
